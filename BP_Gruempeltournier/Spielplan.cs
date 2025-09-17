96e75e0f83bf9f704226819415f11dbb0b17ef05
Julian Graf <graf_julian@icloud.com>
2025-09-17 14:57:40 +0200

    Spielplan Überprüfung

---
+++ BP_Gruempeltournier/Spielplan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP_Gruempeltournier
+{
+    internal class Spielplan
+    {
+        internal static void Generieren()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("      Spielplan erstellen     ");
+            Console.WriteLine("------------------------------");
+
+            while (true)
+            {
+                Console.Write("Spieldauer in Minuten: ");
+                if (!int.TryParse(Console.ReadLine(), out var spieldauer))
+                {
+                    Console.WriteLine("Bitte einen Wert zwischen 5 und 90 eingeben.");
+                    continue;
+                }
+
+                if (spieldauer > 90)
+                {
+                    Console.WriteLine("Die maximale Spielzeit beträgt 90 Minuten.");
+                }
+                else if (spieldauer < 5)
+                {
+                    Console.WriteLine("Die minimale Spielzeit beträgt 5 Minuten.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}

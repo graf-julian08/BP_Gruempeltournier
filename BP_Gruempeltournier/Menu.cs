dd0202553bbb62382d6bb50974df22d66d8a69ec
Julian Graf <graf_julian@icloud.com>
2025-09-17 09:42:42 +0200

    Team erfassen

--- BP_Gruempeltournier/Menu.cs
+++ BP_Gruempeltournier/Menu.cs
@@ -4,6 +4,11 @@
     {
         internal static void CreateMenu()
         {
+
+            bool gameState = true;
+
+            while (gameState) {
+
             Console.WriteLine("|            MENU            |");
             Console.WriteLine("| -------------------------- |");
             Console.WriteLine("| (1) Spieler erfassen       |");
@@ -11,50 +16,70 @@
             Console.WriteLine("| (2) Team erfassen          |");
             Console.WriteLine("|                            |");
             Console.WriteLine("| (3) Spielplan generieren   |");
+            Console.WriteLine("|                            |");
+            Console.WriteLine("| (4) Tournier beenden       |");
             Console.WriteLine("+----------------------------+");
 
             bool correctNumber = false;
 
-            while (!correctNumber)
-            {
-                Console.Write("\nBitte 1, 2 oder 3 eingeben: ");
-                string menuInput = Console.ReadLine();
-
-                if (menuInput == "1" || menuInput == "2" || menuInput == "3")
+                while (!correctNumber)
                 {
-                    correctNumber = true;
-                    if (menuInput == "1")
-                    {
-                        Console.WriteLine("");
-                        Console.WriteLine("       Spieler erfassen       ");
-                        Console.WriteLine("------------------------------");
-                        Console.Write("Vorname: ");
-                        string spielerVorname = Console.ReadLine();
-                        Console.Write("Name: ");
-                        string spielerName = Console.ReadLine();
-                        Console.Write("Geburtstag (tt.mm.jjjj): ");
-                        string spielerGeburtstag = Console.ReadLine();
-                        Console.Write("Wohnort: ");
-                        string spielerWohnort = Console.ReadLine();
+                    Console.Write("\nBitte 1, 2, 3 oder 4 eingeben: ");
+                    string menuInput = Console.ReadLine();
 
-                        Console.WriteLine(spielerVorname + " " + spielerName + " " + spielerGeburtstag + " " + spielerWohnort);
-                    }
-                    else if (menuInput == "2")
+                    if (menuInput == "1" || menuInput == "2" || menuInput == "3" || menuInput == "4")
                     {
-                        Console.WriteLine("");
-                        Console.WriteLine("         Team erfassen        ");
-                        Console.WriteLine("------------------------------");
-                        Console.Write("Teamname: ");
-                        string teamName = Console.ReadLine();
-                        Console.WriteLine("Spieler (kommagetrennt): ");
-                        string teamSpieler = Console.ReadLine();
+                        correctNumber = true;
 
-                        Console.WriteLine(teamName + " " + teamSpieler);
+                        if (menuInput == "1")
+                        {
 
-                    }
-                    else
-                    {
-                        Console.WriteLine("3");
+                            List<string> spielerListe = new List<string>();
+
+                            Console.WriteLine("");
+                            Console.WriteLine("       Spieler erfassen       ");
+                            Console.WriteLine("------------------------------");
+                            Console.Write("Vorname: ");
+                            string spielerVorname = Console.ReadLine();
+                            Console.Write("Name: ");
+                            string spielerName = Console.ReadLine();
+                            Console.Write("Geburtstag (tt.mm.jjjj): ");
+                            string spielerGeburtstag = Console.ReadLine();
+                            Console.Write("Wohnort: ");
+                            string spielerWohnort = Console.ReadLine();
+
+                            spielerListe.Add(spielerVorname + " " + spielerName + " " + spielerGeburtstag + " " + spielerWohnort);
+                            spielerVorname = "";
+                            spielerName = "";
+                            spielerGeburtstag = "";
+                            spielerWohnort = "";
+
+                            Console.WriteLine(spielerListe.Count());
+
+
+                        }
+                        else if (menuInput == "2")
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("         Team erfassen        ");
+                            Console.WriteLine("------------------------------");
+                            Console.Write("Teamname: ");
+                            string teamName = Console.ReadLine();
+                            Console.WriteLine("Spieler (kommagetrennt): ");
+                            string teamSpieler = Console.ReadLine();
+
+                            Console.WriteLine(teamName + " " + teamSpieler);
+
+                        }
+                        else if (menuInput == "3")
+                        {
+                            Console.WriteLine("3");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Spiel beendet!");
+                            gameState = false;
+                        }
                     }
                 }
             }

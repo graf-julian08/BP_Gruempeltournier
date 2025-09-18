c1db5919f5b12df0b1c24f9acb171324a5bb7d86
Philipp Crista <Philipp.crista@gmail.com>
2025-09-18 11:09:09 +0200

    Mergeproblem

--- BP_Gruempeltournier/Spielplan.cs
+++ BP_Gruempeltournier/Spielplan.cs
@@ -4,6 +4,7 @@
     {
         internal static void Generieren()
         {
+            int spielNummer = 1;
             Console.WriteLine("");
             Console.WriteLine("      Spielplan erstellen     ");
             Console.WriteLine("------------------------------");
@@ -20,16 +21,19 @@
                 if (spieldauer > 90)
                 {
                     Console.WriteLine("Die maximale Spielzeit beträgt 90 Minuten.");
+                    continue;
                 }
                 else if (spieldauer < 5)
                 {
                     Console.WriteLine("Die minimale Spielzeit beträgt 5 Minuten.");
+                    continue;
                 }
-                else
-                {
-                    break;
-                }
-                Console.Write("Spieldauer in Minuten: ");
+                break;
+            }
+
+            while (true)
+            {
+                Console.Write("Pausendauer in Minuten: ");
                 if (!int.TryParse(Console.ReadLine(), out var pausenDauer))
                 {
                     Console.WriteLine("Bitte einen Wert zwischen 1 und 15 eingeben!");
@@ -39,15 +43,18 @@
                 if (pausenDauer > 15)
                 {
                     Console.WriteLine("Die maximale Pausenzeit beträgt 15 Minuten!");
+                    continue;
                 }
                 else if (pausenDauer < 1)
                 {
                     Console.WriteLine("Die minimale Pausenzeit beträgt 1 Minute!");
+                    continue;
                 }
-                else
-                {
-                    break;
-                }
+                break;
+            }
+
+            while (true)
+            {
                 Console.Write("Spielbeginn (z.B. 8.00): ");
                 if (!Double.TryParse(Console.ReadLine(), out var spielBeginn))
                 {
@@ -55,19 +62,24 @@
                     continue;
                 }
 
+                spielBeginn = Math.Round(spielBeginn, 2);
+
                 if (spielBeginn > 18.00)
                 {
                     Console.WriteLine("Das letzte Spiel darf spätestens um 18:00 beginnen.");
+                    continue;
                 }
                 else if (spielBeginn < 8.00)
                 {
                     Console.WriteLine("Das Tournier beginnt erst um 8:00 Uhr");
+                    continue;
                 }
-                else
-                {
-                    break;
-                }
+                break;
             }
+            
+            Console.WriteLine("Zwei Teams werden gezogen...");
+            Thread.Sleep(2000);
+            Console.WriteLine($"Spiel {spielNummer}: ... spielt gegen .... Spielstart: {spielBeginn}, Spieldauer: {Spieldauer}, Pausendauer: {pausenDauer}.");
         }
     }
 }

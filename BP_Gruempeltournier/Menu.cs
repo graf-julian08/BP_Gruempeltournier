cde328e6821d9f2c6cb0380312149d3cc2a5a5f5
Julian Graf <graf_julian@icloud.com>
2025-09-19 08:29:16 +0200

    Menu optimiert

--- BP_Gruempeltournier/Menu.cs
+++ BP_Gruempeltournier/Menu.cs
@@ -1,14 +1,6 @@
 ﻿using BP_Gruempeltournier.Data;
-<<<<<<< .merge_file_3Hzkhk
-using BP_Gruempeltournier.Models;
-using System.Globalization;
-
-=======
->>>>>>> .merge_file_SmN2VF
 namespace BP_Gruempeltournier
 {
-
-    
     internal class Menu
     {
         internal static void CreateMenu()
@@ -34,49 +26,7 @@ namespace BP_Gruempeltournier
                 bool validInput = false;
                 string menuInput = "";
 
-                while (!validInput)
-                {
-                    Console.Write("\nBitte 1, 2, 3 oder 4 eingeben: ");
-                    var input = Console.ReadLine();
-
-                    if (input is "1" or "2" or "3" or "4")
-                    {
-                        menuInput = input;
-                        validInput = true;
-                    }
-                }
-
-                switch (menuInput)
-                {
-                    case "1":
-                        SpielerPruefung.ErfasseSpieler(spielerRepo);
-                        break;
-
-                    case "2":
-                        TeamPruefung.ErfasseTeam(spielerRepo, teamRepo);
-                        break;
-
-                    case "3":
-                        SpielplanPruefung.SpielplanPruefen(gameState);
-                        break;
-
-                    case "4":
-                        ConsoleHelper.WriteLineColored("Spiel beendet!", ConsoleColor.Blue);
-                        gameState = false;
-                        break;
-                }
-
-<<<<<<< .merge_file_3Hzkhk
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("Spiel beendet!");
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    gameState = false;
-                    
-                }
-=======
->>>>>>> .merge_file_SmN2VF
+                MenuPruefung.MenuPunkte(ref gameState, spielerRepo, teamRepo, ref validInput, ref menuInput);
             }
         }
     }
